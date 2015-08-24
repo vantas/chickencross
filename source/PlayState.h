cd17124db4d2d434c1b69c66156903d2092e39da
Guilherme Taschetto <gtaschetto@gmail.com>
2015-08-24 18:40:09 -0300

    Add example.

---
+++ source/PlayState.h
@@ -0,0 +1,55 @@
+/*
+ *  PlayState.h
+ *  Normal "play" state
+ *
+ *  Created by Marcelo Cohen on 08/13.
+ *  Copyright 2013 PUCRS. All rights reserved.
+ *
+ */
+
+#ifndef PLAY_STATE_H_
+#define PLAY_STATE_H_
+
+#include "GameState.h"
+#include "Sprite.h"
+#include "InputManager.h"
+
+class PlayState : public cgf::GameState
+{
+    public:
+
+    void init();
+    void cleanup();
+
+    void pause();
+    void resume();
+
+    void handleEvents(cgf::Game* game);
+    void update(cgf::Game* game);
+    void draw(cgf::Game* game);
+
+    // Implement Singleton Pattern
+    static PlayState* instance()
+    {
+        return &m_PlayState;
+    }
+
+    protected:
+
+    PlayState() {}
+
+    private:
+
+    static PlayState m_PlayState;
+
+    int x, y;
+    int dirx, diry;
+    cgf::Sprite playSprite1;
+    cgf::Sprite playSprite2;
+    cgf::Sprite playSprite3;
+    cgf::Sprite player;
+    sf::RenderWindow* screen;
+    cgf::InputManager* im;
+};
+
+#endif
