3a6033c042ccf3201481fecc8dea7c37619fce10
Anderson Fraga <contato@andersonfraga.net>
2015-10-19 00:15:53 -0200

    Adicionado chicken sprite

--- source/PlayState.cpp
+++ source/PlayState.cpp
@@ -20,8 +20,11 @@ using namespace std;
 
 void PlayState::init()
 {
-  playSprite1.load("data/img/Char14.png");
+  playSprite1.load("data/maps/chicken.png", 32, 32, 20, 32, 8, 24, 3, 4, 12);
+  playSprite1.loadAnimation("data/maps/chicken.xml");
   playSprite1.setPosition(400,550);
+  //playSprite1.setAnimRate(1);
+  playSprite1.setAnimation("walk-up");
 
   dirx = 0; // sprite direction: right (1), left (-1)
   diry = 0; // down (1), up (-1)
@@ -66,17 +69,25 @@ void PlayState::handleEvents(cgf::Game* game)
 
   dirx = diry = 0;
 
-  if(im->testEvent("left"))
+  if(im->testEvent("left")) {
+    playSprite1.setAnimation("walk-left");
     dirx = -1;
+  }
 
-  if(im->testEvent("right"))
+  if(im->testEvent("right")) {
+    playSprite1.setAnimation("walk-right");
     dirx = 1;
+  }
 
-  if(im->testEvent("up"))
+  if(im->testEvent("up")) {
+    playSprite1.setAnimation("walk-up");
     diry = -1;
+  }
 
-  if(im->testEvent("down"))
+  if(im->testEvent("down")) {
+    playSprite1.setAnimation("walk-down");
     diry = 1;
+  }
 
   if(im->testEvent("quit") || im->testEvent("rightclick"))
     game->quit();
@@ -98,6 +109,8 @@ void PlayState::update(cgf::Game* game)
   }
 
   playSprite1.setPosition(x,y);
+  playSprite1.update(game->getUpdateInterval());
+  //playSprite1.play();
   player.update(game->getUpdateInterval());
 }
 
